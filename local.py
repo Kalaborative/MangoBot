a716e2eaa723aec6076011343f53abd25a0978bc
Kalaborative <kalaborative94@gmail.com>
2022-04-25 12:37:36 -0500

    New

--- local.py
+++ local.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from discord.ui import Button 
+from discord.ui import Button, View
 import requests
 import json
 from random import choice
@@ -24,6 +24,11 @@ import asyncio
 #     t.start()
 
 db = {}
+coinAmts = {
+
+}
+
+# discord.ButtonStyle().primary
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -52,10 +57,33 @@ class MyBot(commands.Bot):
     
     async def on_ready(self):
         ch = self.get_channel(965308897256702046)
+        print("Ready!")
         await ch.send(f"logged in as {self.user}")
-        # print("Ready!")
+
 # keep_alive()
 # bot.load_extension('jishaku')
 bot = MyBot()
+
+@bot.command()
+async def hello(ctx):
+    button1 = Button(label='Click me!', style=discord.ButtonStyle.green)
+    button2 = Button(emoji='🍦', style=discord.ButtonStyle.primary)
+    button3 = Button(label='Go to Google', url='https://google.com')
+    view = View()
+    view.add_item(button1)
+    view.add_item(button2)
+    view.add_item(button3)
+    await ctx.send("Hi!", view=view)
+
+@bot.command()
+async def bal(ctx):
+    q_author = ctx.author
+    if q_author not in coinAmts.keys():
+        coinAmts[q_author] = [0, 0]
+    wallet = coinAmts[q_author][0]
+    bank = coinAmts[q_author][1]
+    myEmbed = discord.Embed(color=discord.Color.blue(), title=f'{ctx.author}', description=f'Wallet: {wallet} | Bank {bank}')
+    await ctx.send(embed=myEmbed)
+
 bot.run('OTY1MzA4MDQ1OTI2MjAzNDkz.YlxTLA.XTe-rt2ltE0ZpPJyjUoO0jF4BnI')
 # client.run('OTY1MzA4MDQ1OTI2MjAzNDkz.YlxTLA.XTe-rt2ltE0ZpPJyjUoO0jF4BnI')
